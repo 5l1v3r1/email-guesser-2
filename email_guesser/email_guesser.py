1e75d54c45c752c99d3b07446bdc6185d45e3957
Michal Mokrogulski <michal.mokrogulski@gmail.com>
2017-05-28 11:47:52 +0200

    Email guesser guess by list of modules

--- email_guesser/email_guesser.py
+++ email_guesser/email_guesser.py
@@ -1,22 +1,20 @@
+from email_guesser import name_guesser
 
 class EmailGuesser:
-    def __init__(self, guessers_names=None):
-        self.guessers = self.generate_guessers(guessers_names) if guessers_names else self.defaults_guessers()
+    def __init__(self):
+        self.defaults_guessers()
 
     def set_email(self, email):
         self.email = email
+        for guesser in self.guessers:
+            guesser.set_email(email)
 
     def guess(self):
-        """guess method try to guess gender by list of modules
-        :return: "male", "female", None
-        """
-        # TODO
-        pass
+        for guesser in self.guessers:
+            result = guesser.guess()
+            if result is not None:
+                return result
+        return None
 
     def defaults_guessers(self):
-        #TODO
-        return ["", "a"]
-
-    def generate_guessers(self, guessers_names):
-        # TODO
-        return 0
+        self.guessers.append(name_guesser.NameGuesser())
